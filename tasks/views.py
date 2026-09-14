8063bbdd9f45e4f82082fed8c4d241e29d776f1c
Arsalan-497 <Arsalanofficial497@gmail.com>
2026-09-14 14:08:49 +0500

    Initial Django task manager application

---
+++ tasks/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import TaskForm
+from .models import Task
+
+
+def task_list(request):
+    tasks = Task.objects.all().order_by("-created_at")
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+
+    else:
+        form = TaskForm()
+
+    return render(request, "tasks/task_form.html", {"form": form})
+
+
+def task_toggle(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    task.completed = not task.completed
+    task.save()
+
+    return redirect("task_list")
+
+
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+
+    return redirect("task_list")
